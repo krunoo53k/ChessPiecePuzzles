e7010ac248b9ca64d4feb6e36c789552f2cce4b1
krunoo53k <petrik.kpetrik@gmail.com>
2023-03-20 14:46:09 +0100

    Adding a print function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include "ChessPieceClasses.cpp"
 
-int main(){
+int main()
+{
+
+    ChessPiece* chessBoard[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%c",chessBoard[i][j]->getSymbol());
+        }
+        printf("\n");
+    }
+
     return 0;
 }
